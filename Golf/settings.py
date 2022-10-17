c9df44638951ac454fe3e99720759de958045472
CarlGauss1 <eharty1@outlook.com>
2022-10-17 22:57:37 +0000

    deployment

--- Golf/settings.py
+++ Golf/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['golfstar.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['golfstar.herokuapp.com', 'localhost']
 
 
 # Application definition
