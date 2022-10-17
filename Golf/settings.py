98169da672f056c8a686337d0ae9bc669f1d08b6
CarlGauss1 <eharty1@outlook.com>
2022-10-17 21:42:00 +0000

    deployment commit

--- Golf/settings.py
+++ Golf/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'Golf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIRS],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
