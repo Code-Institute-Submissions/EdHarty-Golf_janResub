73e6bd859b46efcbc8b75704c70ec5d76ea54f78
CarlGauss1 <eharty1@outlook.com>
2022-10-20 11:52:34 +0000

    Add edits

--- app/forms.py
+++ app/forms.py
@@ -13,7 +13,7 @@ class UpdateTeetimeDetails(forms.ModelForm):
 class EditAccountForm(forms.ModelForm):
 
     phone_number = CharField(widget=TextInput(attrs={'type': 'number'}))
-    
+   
     class Meta:
         model = UserAccount()
         fields = ('first_name', 'last_name', 'phone_number')
