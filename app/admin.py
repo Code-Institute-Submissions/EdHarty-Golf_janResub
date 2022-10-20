e3bbce5e493f809e7854b5a689bfbb1fbcd215a4
CarlGauss1 <eharty1@outlook.com>
2022-10-20 15:10:22 +0000

    Add classes for admin site

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Teetime, UserAccount
+
+
+@admin.register(Teetime)
+class TeetimeAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'teetime_time', 'teetime_date')
+    readonly_fields = ('teetime_id')
+    list_display = ('teetime_id', 'user', 'teetime_date', 'teetime_time', 'player_count',
+     'status', 'created_on')
+    search_fields = ('teetime_id', 'user')
+    actions = ['approve_teetime']
+
+    def approve_teetime(self, queryset):
+        queryset.update(approve_teetime=True)
+
+
+@admin.register(UserAccount)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name', 'phone_number')
+    search_fields = ('user', 'phone_number')
