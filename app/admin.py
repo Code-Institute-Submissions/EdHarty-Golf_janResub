85615a0366bfdfc8708f00b6027040b8fcaaa6e0
CarlGauss1 <eharty1@outlook.com>
2022-11-23 18:49:50 +0000

    Add env.py file, DATABASE_URL, SECRET_KEY

--- app/admin.py
+++ app/admin.py
@@ -6,7 +6,7 @@ from .models import Teetime, UserAccount
 @admin.register(Teetime)
 class TeetimeAdmin(admin.ModelAdmin):
     list_filter = ('status', 'teetime_time', 'teetime_date')
-    readonly_fields = ('teetime_id')
+    readonly_fields = ('teetime_id',)
     list_display = ('teetime_id', 'user', 'teetime_date', 'teetime_time',
                     'player_count', 'status', 'created_on')
     search_fields = ('teetime_id', 'user')
