867890f6ff20dd720ba471ea71f98952f1bbcc2f
CarlGauss1 <eharty1@outlook.com>
2023-01-09 21:38:06 +0000

    Added env.py settings and editted settings

--- Golf/urls.py
+++ Golf/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.url'), name='home'),
+    path('', include('app.urls'), name='home'),
     path('accounts/', include('allauth.urls')),
    ]
